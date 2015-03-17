42de7aa86f4a670df80b63ce8e4890d8c1403158
galpHub <galpHub@users.noreply.github.com>
2015-03-17 04:26:20 -0600

    Added code for merging data

--- run_analysis.R
+++ run_analysis.R
@@ -12,16 +12,42 @@ dataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%2
 
 download(dataUrl,destfile= destFile)
 dateDownloaded <- Sys.time()
+
+
+## Part1: Reading and merging
+
+## Read the file into R
 unzip(destFile)
-
-
-######## Read the file into R
 nameData <- "./UCI HAR Dataset/features.txt"
-trainDataDir <- "./UCI HAR Dataset/train"
-testDataDir <- "./UCI HAR Dataset/test"
+trainDataDir <- "./UCI HAR Dataset/train/X_train.txt"
+testDataDir <- "./UCI HAR Dataset/test/X_test.txt"
 
 ## Read names of variables.
 name_list <- read.table(nameData,header=FALSE,colClasses=c("numeric","character"),
 				stringsAsFactors=FALSE)
 names(name_list)<- c("VarNumber","VarName")
 namesOfVars <- name_list$VarName
+
+## Reading the data
+trainData <- read.table(trainDataDir,header=FALSE,colClasses = c("numeric"),
+				stringsAsFactor = FALSE)
+names(trainData)<- namesOfVars
+
+testData <- read.table(testDataDir,header=FALSE,colClasses = c("numeric"),
+				stringsAsFactor = FALSE)
+names(testData)<- namesOfVars
+
+## Cleaning up the data
+unlink("UCI HAR Dataset",recursive=TRUE)
+
+## Subsetting the data and merging
+tags <- c("-mean()","-std()")
+indices_mean <- grepl(tags[1],namesOfVars,fixed=TRUE)
+indices_std <- grepl(tags[1],namesOfVars,fixed=TRUE)
+indices <- indices_mean+indices_std>0
+
+data <- merge(trainData[indices,],testData[indices,])
+
+
+
+
