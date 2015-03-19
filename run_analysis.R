c8fd59a7c4c92387f7a6b9383dbf68fbd8dedd07
galpHub <galpHub@users.noreply.github.com>
2015-03-19 10:53:57 -0600

    Added subject Ids and fixed VarNames typo

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+## Creating a tidy directory
 library(downloader)
 
 newDirectory <- "./CleanData/Week3/smartPhoneData"
@@ -6,11 +7,13 @@ if(!dir.exists(newDirectory)){
 }
 setwd(newDirectory)
 
-
+## Downloading the data file.
 destFile <- "smartphone.zip"
 dataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+if(!file.exists(destFile)){
+	download(dataUrl,destfile= destFile)
+}
 
-download(dataUrl,destfile= destFile)
 dateDownloaded <- Sys.time()
 
 
@@ -32,23 +35,33 @@ testLabelsDir <- "./UCI HAR Dataset/test/y_test.txt"
 
 trainLabels <- read.table(trainLabelsDir,header=FALSE,colClasses=c("numeric"))$V1
 testLabels <- read.table(testLabelsDir,header=FALSE,colClasses=c("numeric"))$V1
-dataLabels <- rbind(trainLabels, testLabels)
+dataLabels <- c(trainLabels, testLabels)
+
+## Read the numeric identifiers of the subjects
+trainSubjectDir<-"./UCI HAR Dataset/train/subject_train.txt"
+testSubjectDir <- "./UCI HAR Dataset/test/subject_test.txt"
+trainSubjectId <-read.table(trainSubjectDir,colClasses="integer")
+testSubjectId <- read.table(testSubjectDir,colClasses="integer")
 
 
-## Read names of variables.
+## Read variables names inro R.
 name_list <- read.table(nameData,header=FALSE,colClasses=c("numeric","character"),
 				stringsAsFactors=FALSE)
 names(name_list)<- c("VarNumber","VarName")
 namesOfVars <- name_list$VarName
 
-## Reading the data
+## Reading the data, adding names and adding subject ids
 trainData <- read.table(trainDataDir,header=FALSE,colClasses = c("numeric"),
 				stringsAsFactor = FALSE)
 names(trainData)<- namesOfVars
+trainData$subjectId <- trainSubjectId
 
 testData <- read.table(testDataDir,header=FALSE,colClasses = c("numeric"),
 				stringsAsFactor = FALSE)
 names(testData)<- namesOfVars
+testData$subjectId <- testSubjectId
+
+
 
 ## Cleaning up the data
 unlink("UCI HAR Dataset",recursive=TRUE)
@@ -62,12 +75,28 @@ indices <- indices_mean+indices_std>0
 data <- rbind(trainData[,indices],testData[,indices])
 data$Activity <- dataLabels
 
-## Showing data who's boss! (i.e. Tidying)
 
+
+## Tidying the data
+## Renaming activities from numeric lables to descriptive strings
  labelVector <- c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS",
 	"SITTING", "STANDING", "LAYING")
-## labelVector is to be used as follows (to improve readability):
-#data$label <- sapply(dataLabels, function(x) labelVector[x])
+data$Activity <- sapply(dataLabels, function(x) labelVector[x])
+
+
+## In renaming the variable names, it is worth noting that there are
+## some variable names in this data set that do not seem to correspond
+## to the prescribed names in the markdown that accompanies the dataset.
+## In particular some variables have accidentally used the substring
+## "Body" twice in a row. This happens for the variables whose name,
+## according to the markdown, should be one of the following:
+## 	1)"fBodyAccJerkMag"
+##	2)"fBodyGyroMag"
+##	3)"fBodyGyroJerkMag"
+## In the provided data set, some variables have these names and others
+## have the suspiciously similar names where the substring body appears
+## twice and consecutively, e.g. 1)"fBodyBodyAccJerkMag". No such variable
+## is described in the markdown, leading me to suspect that it is a typo.
 
 newnames<-c(
 "LinearAcceleration",
@@ -88,10 +117,14 @@ newnames<-c(
 "FT_AngularVelocity_Magnitude",
 "FT_AngularVelocity_JerkMagnitude")
 
+newnameFixTypo <- newnames[15:17]
 
+newnames <- c(newnames,newnameFixTypo)
 
 ## The oldnames could be extracted algorithmically more cleanly perhaps,
-## although the newnames would be just as ugly.
+## although the newnames would be just as ugly. Also, there is a typo in
+## some of the variables were the substring 'Body' is repeated twice,
+## unlike any of the variable names in the accompanying markdown file.
 oldnames<-c(
 "tBodyAcc",
 "tGravityAcc",
@@ -100,31 +133,42 @@ oldnames<-c(
 "tBodyGyroJerk",
 "tBodyAccMag",
 "tGravityAccMag",
-"tBodyAccJerkMag",
+"tBodyAccJerkMag",  
 "tBodyGyroMag",
 "tBodyGyroJerkMag",
 "fBodyAcc",
 "fBodyAccJerk",
 "fBodyGyro",
 "fBodyAccMag",
-"fBodyAccJerkMag",
-"fBodyGyroMag",
-"fBodyGyroJerkMag")
+"fBodyAccJerkMag",  ## Typo in actual variable names. Uses body twice. 15
+"fBodyGyroMag",	  ## Typo in actual variable names. Uses body twice. 16
+"fBodyGyroJerkMag") ## Typo in actual variable names. Uses body twice. 17
+
+oldnameTypos <- sapply(15:17, function(x) gsub("fBody","fBodyBody",oldnames[x]))
+
+oldnames <- c(oldnames,oldnameTypos)
 
 
-
-## Note that we need to order them by length so that variable 
-## names which are substrings dont get overwritten the wrong way.
-## Also there is a typo in some of the variable names - the word
-## body is repeated twice.
+## Note that we need to order them by decreasing length so that variable 
+## names which are substrings of other names dont get overwritten 
+## the wrong way.
 reorderNames <- order(nchar(oldnames),oldnames,decreasing=TRUE)
 oldnames<-oldnames[reorderNames]
 newnames<-newnames[reorderNames]
 
-
+## Rename the variables in nameOfVars 
 for(j in 1:length(oldnames)){
 	namesOfVars <- gsub(oldnames[j],newnames[j],namesOfVars,fixed= TRUE)
 }
 
+## Check that the renaming of the relevant variables was successful
+
+wrong_nameIndex <- namesOfVars[indices]==name_list[indices,]$VarName
+number_ofErrors <- sum(wrong_nameIndex)
+if(number_ofErrors>0){
+	print("Names were not all correctly changed")
+}else{
+	print("All names were correctly changed!")
+}
 
 
